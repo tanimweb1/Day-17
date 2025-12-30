ffde8a4452deb4e4f8f9438adb00092775633e7e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2025-12-31 01:26:49 +0600

    Update Day-17.c

--- Day-17.c
+++ Day-17.c
@@ -1,9 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
 
+    // prime numnber
+    int n;
+    scanf("%d", &n);
+    int prime = 0;
 
-// Break and continue statement
+    for (int i = 2; i < n; i++)
+    {
+
+        if (n % i == 0)
+        {
+
+            prime++;
+            break;
+        }
+    }
+
+    if (prime == 0)
+        printf("Prime number");
+    else
+        printf("Not a prime number");
+
+   
+   
+   
+   
+   
+   // Break and continue statement
 for(int i = 1; i<=35; i++){
     
 if(i%3==0)
@@ -28,36 +54,24 @@ for(int i = 1; i<=10; i++){
 printf("%d\n", n*i);
 }
 
-
-    // Break and continue statement
-for(int i = 1; i<=35; i++){
-    
-if(i%3==0)
-continue;
-printf("%d\n",i);
-
-if(i==10)
-break;
-
-}
-
-
-
-// Multiplication Table
+// Factorial
 int n;
-scanf("%d", &n);
-int table=0;
-
-for(int i = 1; i<=10; i++){
- 
-
-printf("%d\n", n*i);
+scanf("%d",&n);
+int fact=1;
+for(int i =1; i<=n;i++){
+    fact = fact*i;
 }
+printf("Facto is %d", fact);
 
 
-
-
-
-    return 0;
-
+   
+   
+   
+   
+   
+   
+   
+   
+   
+        return 0;
 }
