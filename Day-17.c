2a54d54a7bd755ac3fd1e9f25f30ee2d0107ded5
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2025-12-30 23:58:08 +0600

    Add files via upload

---
+++ Day-17.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+
+
+// Break and continue statement
+for(int i = 1; i<=35; i++){
+    
+if(i%3==0)
+continue;
+printf("%d\n",i);
+
+if(i==10)
+break;
+
+}
+
+
+
+// Multiplication Table
+int n;
+scanf("%d", &n);
+int table=0;
+
+for(int i = 1; i<=10; i++){
+ 
+
+printf("%d\n", n*i);
+}
+
+
+
+
+
+    return 0;
+}
